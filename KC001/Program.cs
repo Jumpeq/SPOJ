76633401288175aaa1309438665c965964703fb7
Jumpeq <33371237+Jumpeq@users.noreply.github.com>
2018-02-21 11:02:27 +0100

    KC001

---
+++ KC001/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+//Napisz program, który oblicza sumę trzech liczb całkowitych.
+
+//Wejście
+//Na wejście programu podane zostaną trzy liczby 
+//całkowite(nieprzekraczające 100) rozdzielone znakiem nowej linii.
+
+//Wyjście
+//Na wyjściu ma się pojawić suma liczb, które pojawiły się na wejściu.
+
+//Przykład
+//Wejście:
+//100
+//-68
+//12
+
+
+//Wyjście:
+//44
+namespace KC001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(y1 + y2 + y3);
+        }
+    }
+}
